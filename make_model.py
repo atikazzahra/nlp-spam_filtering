b2c315565b72543d437b35868ea385719da0177d
atikazzahra <atikazzahraa@gmail.com>
2017-08-30 15:56:19 +0700

    import pandas

--- make_model.py
+++ make_model.py
@@ -1,8 +1,9 @@
 from collections import Counter
 import numpy as np
+import pandas as pd
 
-def make_Dictionary(train_dir):
-	dataset = open( train_dir, "r" )
+def make_Dictionary(train_file):
+	dataset = open( train_file, "r" )
 	all_words = []
 	for line in dataset:
 	    words = line.split()
@@ -22,18 +23,54 @@ def make_Dictionary(train_dir):
 
 	return dictionary
 
-def extract_Features(mail_dir):
+def extract_Features(mail_dir, dic):
 	dataset = open( mail_dir, "r" )
-	feature_matrix = np.zeros(len(dataset), 3000)
-	
-	for idxline, line in dataset:
+	len_dataset = (sum(1 for _ in dataset))
+
+	feature_matrix = np.zeros((len_dataset, 3000))
+	label_matrix = []
+	for idxline, line in enumerate(dataset):
 		words = line.split()
 		label = words.pop(0)
+		label_matrix += label
 		for word in words:
-			for idxdict, val in enumerate(dictionary):
-				if val[0] == word:
+			for idxdict, val in enumerate(dic):
+				if val == word:
 					feature_matrix[idxline, idxdict] = words.count(word)
-	return feature_matrix;
+	return feature_matrix, label_matrix
 
 
-print (make_Dictionary("dataset"))
+# dictionary = make_Dictionary("dataset")
+# # feature_data, label_data = extract_Features("dataset", dictionary)
+
+# # print(feature_data)
+# # f = open('hasil', 'w')
+# # np.set_printoptions(threshold=np.nan)
+# # f.write(np.array_str(feature_data[0, :]))
+# # f.close()
+# # dataset = open( "dataset", "r" )
+# # print (sum(1 for _ in dataset))
+# # with dataset as ln:
+# # 	print (sum(1 for _ in ln))
+
+# dataset = open( "dataset", "r" )
+
+# with 
+# 	len_dataset = (sum(1 for _ in dataset))
+
+# feature_matrix = np.zeros((len_dataset, 3000))
+# label_matrix = []
+
+
+# for idxline, line in enumerate(dataset):
+# 	words = line.split()
+# 	label = words.pop(0)
+# 	label_matrix += label
+# 	for word in words:
+# 		print (word)
+# 		for idxdict, val in enumerate(dictionary):
+# 			if val == word:
+# 				feature_matrix[idxline, idxdict] = words.count(word)
+
+dataset = pd.read_csv("dataset", sep='/t', error_bad_lines=True)
+print(dataset.head)
