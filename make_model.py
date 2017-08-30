193a3300c40f0724372c97266c31e5cb13936158
atikazzahra <atikazzahraa@gmail.com>
2017-08-30 08:41:33 +0700

    add extract feature function

--- make_model.py
+++ make_model.py
@@ -1,4 +1,6 @@
 from collections import Counter
+import numpy as np
+
 def make_Dictionary(train_dir):
 	dataset = open( train_dir, "r" )
 	all_words = []
@@ -19,5 +21,19 @@ def make_Dictionary(train_dir):
 	dictionary = dictionary.most_common(3000)
 
 	return dictionary
+
+def extract_Features(mail_dir):
+	dataset = open( mail_dir, "r" )
+	feature_matrix = np.zeros(len(dataset), 3000)
+	
+	for idxline, line in dataset:
+		words = line.split()
+		label = words.pop(0)
+		for word in words:
+			for idxdict, val in enumerate(dictionary):
+				if val[0] == word:
+					feature_matrix[idxline, idxdict] = words.count(word)
+	return feature_matrix;
+
 
 print (make_Dictionary("dataset"))
