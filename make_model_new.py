8bb465ceba931b1fb1422ca74a46b1e7498cb8cf
atikazzahra <atikazzahraa@gmail.com>
2017-09-07 07:22:16 +0700

    preprocess per line

--- make_model_new.py
+++ make_model_new.py
@@ -7,9 +7,12 @@ from sklearn.model_selection import cross_val_score
 from sklearn.metrics import confusion_matrix
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
 import re
+import string
+
 def preprocess(train_file):
 	dataset = open( train_file, "r" )
 	all_words = []
@@ -22,42 +25,30 @@ def preprocess(train_file):
 	    words = line.lower().split()
 	    label = words.pop(0)
 	    labels.append(label)
-
-	    # words_temp = []
-	    # for word in words:
-	    #     if word not in stopWords and len(word) != 1:
-	    #         words_temp.append(word)
-
 	    all_words.append(words)
 	dataset.close()
 
-	# for idw, words in enumerate(all_words):
-	# 	ps = PorterStemmer()
-	# 	for idx, word in enumerate(words):
-	# 		all_words[idw][idx] = ps.stem(word)
-
 	return all_words, labels
 
-def preprocess_text(messy_string):
-    assert(type(messy_string) == str)
-    porter = PorterStemmer()
-    stop_words = set(stopwords.words('english'))
-    cleaned = re.sub(r'\b[\w\-.]+?@\w+?\.\w{2,4}\b', 'emailaddr', messy_string)
-    cleaned = re.sub(r'(http[s]?\S+)|(\w+\.[A-Za-z]{2,4}\S*)', 'httpaddr',
-                     cleaned)
-    cleaned = re.sub(r'£|\$', 'moneysymb', cleaned)
-    cleaned = re.sub(
-        r'\b(\+\d{1,2}\s)?\d?[\-(.]?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b',
-        'phonenumbr', cleaned)
-    cleaned = re.sub(r'\d+(\.\d+)?', 'numbr', cleaned)
-    cleaned = re.sub(r'[^\w\d\s]', ' ', cleaned)
-    cleaned = re.sub(r'\s+', ' ', cleaned)
-    cleaned = re.sub(r'^\s+|\s+?$', '', cleaned.lower())
-    return ' '.join(
-        porter.stem(term) 
-        for term in cleaned.split()
-        if term not in set(stop_words)
-    )
+def preprocess_text(text):
+	assert(type(text) == str)
+	wnl = WordNetLemmatizer()
+	ps = PorterStemmer()
+	stop_words = set(stopwords.words('english'))
+	cleaned = re.sub(r'\b[\w\-.]+?@\w+?\.\w{2,4}\b', 'emailaddr', text)
+	cleaned = re.sub(r'(http[s]?\S+)|(\w+\.[A-Za-z]{2,4}\S*)', 'httpaddr',
+	                 cleaned)
+	cleaned = re.sub(r'£|\$', 'moneysymb', cleaned)
+	cleaned = re.sub(
+	    r'\b(\+\d{1,2}\s)?\d?[\-(.]?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\b',
+	    'phonenumbr', cleaned)
+	cleaned = re.sub(r'\d+(\.\d+)?', 'numbr', cleaned)
+	cleaned = re.sub(r'[^\w\d\s]', ' ', cleaned)
+	cleaned = re.sub(r'\s+', ' ', cleaned)
+	cleaned = re.sub(r'^\s+|\s+?$', '', cleaned.lower())
+	cleaned = cleaned.translate(cleaned.maketrans("","", string.punctuation))
+	cleaned = ' '.join(wnl.lemmatize(term) for term in cleaned.split() if term not in set(stop_words))
+	return ' '.join(ps.stem(term) for term in cleaned.split())
 
 def make_Dictionary(dataset, length):
 	all_words = []
@@ -78,16 +69,15 @@ def extract_Features(dataset, dictionary):
 					feature_matrix[idxline, idxdict] = words.count(word)
 	return feature_matrix
 
-#print(preprocess_text('WINNER!! As a valued network customer you have been selected to receivea £900 prize reward! To claim call 09061701461. Claim code KL341. Valid 12 hours only.'))
 data, label_data = preprocess("dataset")
 dictionary = make_Dictionary(data, 3000)
 feature_data = extract_Features(data, dictionary)
 
-f = open('dict', 'w')
+f = open('dictnew', 'w')
 f.write(str(dictionary))
 f.close()
 
-g = open('hasil2', 'w')
+g = open('hasilnew', 'w')
 g.write(str(data))
 g.close()
 
